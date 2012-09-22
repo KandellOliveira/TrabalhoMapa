03edc552833576b6f2f0d4fa372de1e79929bf8a
Vitor Eduardo <vsdvitor@gmaill.com>
2012-09-22 00:08:40 +0000

    Refatoração na estrutura do projeto

--- src/Modelos/Mapa.java
+++ src/Modelos/Mapa.java
@@ -1,25 +1,20 @@
-package Mapa;
+package Modelos;
 
 import java.util.ArrayList;
 
 public class Mapa {
-    ArrayList<Vertice> vertices;
-
+    private ArrayList<Vertice> vertices;
     
     public Mapa(ArrayList<Vertice> vertices) {
         this.vertices = vertices;
     }
     
- public static void main(String[] args){
-     LerXml ler = new LerXml("A:/mapa.xml");
-     try{
-        Mapa mapa = new Mapa(ler.lerMapa());
-        for (Vertice vertices : mapa.vertices) {
-         System.out.println(vertices);
-        }
-     
-     }catch(Exception e){
-         e.printStackTrace();
-     }
- }
+    public ArrayList<Vertice> getVertices(){
+        return this.vertices;
+    }
+    
+    
+   
+    
+ 
 }
